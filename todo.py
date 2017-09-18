5a9811d85d368c0e935f4cf718684feaca5308ce
Alessa Messineo <apmessineo@gmail.com>
2017-09-18 11:07:19 -0400

    modify show_all query

--- todo.py
+++ todo.py
@@ -4,14 +4,17 @@ from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from flask_cors import CORS
 
 from flask import jsonify
 
 
 app=flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# create table todo_list(id integer primary key autoincrement,item text, completed boolean);
 
 class Todo(db.Model):
     __tablename__ = 'todo_list'
@@ -30,9 +33,10 @@ a PUT (for mark item as complete) """
 def show_all():
     # will return all items in todo list
     print "inside show_all"
-    data = db.session.query(Todo).all()
+    data = Todo.query.all()
+
     print data
-    # return jsonify({'todo': data});
+    return jsonify({'todo': data});
 
 @app.route('/new', methods=["POST", "GET"])
 # print "request: " + request
